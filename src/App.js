c0a3eade4958eb59990e39571804a85f5a4640a8
LYNN DAMOTA <bdamota0506@gmail.com>
2020-10-10 02:16:12 -0700

    about section added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header';
+import About from './Components/About';
 import Web from './Components/Web';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
@@ -9,6 +10,7 @@ function App () {
    return (
       <div className="App">
         <Header />
+        <About />
         <Web />
         <Contact />
         <Footer />
---
+++ src/Components/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import aboutImage from "../images/profilepic.jpg"
+
+function About ()  {
+    return (
+      <section id="about">
+      <div className="row">
+         <div className="six columns main-col col text-center">
+            <h2>About Me</h2>
+            <div className="aboutInfo">
+              <p>I have a passion for people and telling diverse stories that reflect both our unique and shared experiences. I've spent the last ten years working with clients to create authentic and compelling broadcast, digital and branded content that resonates with general market and multicultural audiences.</p>
+            <p>I'm currently enrolled in an intensive MERN Stack Coding Bootcamp at UCLA, UX Design Programs through Udacity and General Assembly, and a Digital Product Management Specialization through UVA Darden School of Business. My goal is to acquire the skills needed to manage the development process of polished, fully responsive and interactive digital products that drive user engagement.</p>
+            <p>When I'm not working or trying to learn something new, I most recently can be found in my garden, cooking, or walking my dog around the neighborhood. I'm anxiously awaiting when I can go to concerts and travel abroad again!</p>
+            </div>
+            </div>
+            <div className="six columns">
+            <img className="profile-pic"  src={aboutImage} alt="Briana daMota Profile Pic" />
+         </div>
+         </div>
+   </section>
+    );
+  }
+
+
+export default About;
