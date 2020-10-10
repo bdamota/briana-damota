1b043990df6ad06bbba8637e05447fd155339c9c
LYNN DAMOTA <bdamota0506@gmail.com>
2020-10-10 02:08:21 -0700

    contact section added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header';
+import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 
 function App () {
@@ -8,7 +9,7 @@ function App () {
       <div className="App">
         <Header />
 
-
+        <Contact />
         <Footer />
       </div>
     );
---
+++ src/Components/Contact.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+
+function Contact () {
+    return (
+      <section id="contact">
+        <div className="col text-center">
+         <h2> Say Hello</h2>
+         </div>
+
+         <div className="row section-head">
+
+            <div className="twelve columns col text-center">
+                  <p className="address">
+                  <span> I enjoy creating and collaborating. Please reach out with opportunities or to say hello!</span><br /><br /><br />
+						         Briana daMota<br />
+                     <span className="fa fa-globe"></span> Los Angeles, CA<br />
+						   <span className="fa fa-envelope"></span> <span className="email">bdamota0506@gmail.com</span>
+					   </p>
+
+            </div>
+      </div>
+   </section>
+    );
+  }
+
+
+export default Contact;
