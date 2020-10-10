2cc288625483d7bcbb336deb37b3f11ac24ba92e
LYNN DAMOTA <bdamota0506@gmail.com>
2020-10-10 02:06:30 -0700

    footer added

---
+++ src/Components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Footer () { 
+    return (
+      <footer>
+
+     <div className="row">
+        <div className="twelve columns">
+           <ul className="social-links">
+            <li><a href="https://www.linkedin.com/in/brianadamota/" target="blank"> <i className="fa fa-linkedin"></i> </a></li>
+            <li><a href="https://www.instagram.com/youngbreez/" target="blank"> <i className="fa fa-instagram"></i> </a></li>
+            <li><a href="https://github.com/bdamota" target="blank"> <i className="fa fa-github"></i> </a></li>
+           </ul>
+
+           <ul className="copyright">
+              <li>&copy; Copyright 2020 Briana daMota</li>
+           </ul>
+
+        </div>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+     </div>
+  </footer>
+    );
+  }
+
+
+export default Footer;
