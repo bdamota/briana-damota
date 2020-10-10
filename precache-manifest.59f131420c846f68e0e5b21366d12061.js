fa920bd22d3a536a5a046e6075156dcb53207c76
LYNN DAMOTA <bdamota0506@gmail.com>
2020-10-10 02:55:29 -0700

    Updates

---
+++ precache-manifest.59f131420c846f68e0e5b21366d12061.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa9636a572c19ed1206f185d2d1eaf2",
+    "url": "/briana-damota/index.html"
+  },
+  {
+    "revision": "64f74c8fcb7f6dcae452",
+    "url": "/briana-damota/static/css/main.89188e8d.chunk.css"
+  },
+  {
+    "revision": "2873e04a40c0b0a6cdd5",
+    "url": "/briana-damota/static/js/2.6445ee84.chunk.js"
+  },
+  {
+    "revision": "78e572a38ce62fad1af1cd75e03f15bb",
+    "url": "/briana-damota/static/js/2.6445ee84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64f74c8fcb7f6dcae452",
+    "url": "/briana-damota/static/js/main.5a10b916.chunk.js"
+  },
+  {
+    "revision": "fd26ba629afdd4ec5126",
+    "url": "/briana-damota/static/js/runtime-main.d3b154b4.js"
+  },
+  {
+    "revision": "2f635cdc0631bd6aae63aee5a7c4f114",
+    "url": "/briana-damota/static/media/BiggestLoser.2f635cdc.mp4"
+  },
+  {
+    "revision": "73bdca4586ebe99a8ffe24685e77fc66",
+    "url": "/briana-damota/static/media/Freeform.73bdca45.mp4"
+  },
+  {
+    "revision": "ff0ff002201995a99be8613754742c26",
+    "url": "/briana-damota/static/media/MasQueScreen.ff0ff002.jpg"
+  },
+  {
+    "revision": "44b5a8f8ac305623ac6be7bd08d30c44",
+    "url": "/briana-damota/static/media/Mylingual.44b5a8f8.jpg"
+  },
+  {
+    "revision": "5ec4fc14c31db2ef6906eb2d9fc148d2",
+    "url": "/briana-damota/static/media/Portfolio.5ec4fc14.jpg"
+  },
+  {
+    "revision": "66b063a70f27f14a13e58f95a8cd79f8",
+    "url": "/briana-damota/static/media/Ram.66b063a7.mp4"
+  },
+  {
+    "revision": "8d608110cceccce346a8d68f6b6222d2",
+    "url": "/briana-damota/static/media/Reina.8d608110.mp4"
+  },
+  {
+    "revision": "c780db6c95bbbdba00d4bdee5e37c0c6",
+    "url": "/briana-damota/static/media/TPIR.c780db6c.mp4"
+  },
+  {
+    "revision": "6f352c4ded6197387d1423f2b20a171c",
+    "url": "/briana-damota/static/media/Toyota.6f352c4d.mp4"
+  },
+  {
+    "revision": "a7e193dcd8060d7dcd032b3a595526c3",
+    "url": "/briana-damota/static/media/architecture10.a7e193dc.jpg"
+  },
+  {
+    "revision": "95941d8d0ab1c0dcc0be945b062934bb",
+    "url": "/briana-damota/static/media/architecture11.95941d8d.jpg"
+  },
+  {
+    "revision": "b896594b659ad8ea40221039f0b4098b",
+    "url": "/briana-damota/static/media/architecture12.b896594b.jpg"
+  },
+  {
+    "revision": "0fab1beb9026b0f7be25ac4175241c32",
+    "url": "/briana-damota/static/media/architecture13.0fab1beb.jpg"
+  },
+  {
+    "revision": "290bc16d306a6ebbfdae68c678b339d4",
+    "url": "/briana-damota/static/media/architecture19.290bc16d.jpg"
+  },
+  {
+    "revision": "67b9bc73e2272a3b5514763f84c0ad01",
+    "url": "/briana-damota/static/media/architecturehero.67b9bc73.jpg"
+  },
+  {
+    "revision": "34f7f3082a8e2cea5764d866a83d18fc",
+    "url": "/briana-damota/static/media/bl.34f7f308.jpg"
+  },
+  {
+    "revision": "f3c94ae0cf146f169aa861d1b71d24da",
+    "url": "/briana-damota/static/media/business1.f3c94ae0.jpg"
+  },
+  {
+    "revision": "9fb6baa4d54d0fb9ae6b697d58b4f8ca",
+    "url": "/briana-damota/static/media/business3.9fb6baa4.jpg"
+  },
+  {
+    "revision": "ae3dd62d6347223ac659978f858ec4e1",
+    "url": "/briana-damota/static/media/business5.ae3dd62d.jpg"
+  },
+  {
+    "revision": "d0398ab410ff2e5487fb1c85de507eb2",
+    "url": "/briana-damota/static/media/business6.d0398ab4.jpg"
+  },
+  {
+    "revision": "fae1e71661299c28cf5fcbfd7ce42d60",
+    "url": "/briana-damota/static/media/kickback.fae1e716.jpg"
+  },
+  {
+    "revision": "4fe5776be46e2f3f6dbf6a9db306a57a",
+    "url": "/briana-damota/static/media/profilepic.4fe5776b.jpg"
+  },
+  {
+    "revision": "be5578e4eed24250fcc73ed5bd5df0bd",
+    "url": "/briana-damota/static/media/ram.be5578e4.jpg"
+  },
+  {
+    "revision": "f3a9aa84322ac6d2dffe3d66535d99cd",
+    "url": "/briana-damota/static/media/reina.f3a9aa84.jpg"
+  },
+  {
+    "revision": "c42c3a951e4fc9cf53bd47a20d90dbd4",
+    "url": "/briana-damota/static/media/toyota.c42c3a95.jpg"
+  },
+  {
+    "revision": "6ee8a3e08f1cf8350955f7eebd8f7f13",
+    "url": "/briana-damota/static/media/tpir.6ee8a3e0.jpg"
+  },
+  {
+    "revision": "46aaa485892ec1999083612d800adcd2",
+    "url": "/briana-damota/static/media/travel12.46aaa485.jpg"
+  },
+  {
+    "revision": "143f2f7153bd10bccecf8b0fcfd995df",
+    "url": "/briana-damota/static/media/travel2.143f2f71.jpg"
+  },
+  {
+    "revision": "7b326e94b7187b980b9574ff781017db",
+    "url": "/briana-damota/static/media/travel3.7b326e94.jpg"
+  },
+  {
+    "revision": "e453bd2df4a880a3113cf178c2215218",
+    "url": "/briana-damota/static/media/travel4.e453bd2d.jpg"
+  },
+  {
+    "revision": "7b8e62e0f029b782073b845ff9ce62af",
+    "url": "/briana-damota/static/media/travel5.7b8e62e0.jpg"
+  },
+  {
+    "revision": "dbaf17133d26f33d79dabbd8e15ce887",
+    "url": "/briana-damota/static/media/travel6.dbaf1713.jpg"
+  },
+  {
+    "revision": "97400708add7a0dc98c64a6a966ce7b5",
+    "url": "/briana-damota/static/media/travel8.97400708.jpg"
+  },
+  {
+    "revision": "66cdd0df003c455c42e1100b8e74e575",
+    "url": "/briana-damota/static/media/travelhero.66cdd0df.jpg"
+  }
+]);
